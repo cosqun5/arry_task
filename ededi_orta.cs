faaaef484b62463b5c1610ba6f9b641e224ebc26
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-03-10 10:00:23 +0400

    initial commit

---
+++ ededi_orta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            double[] a = { 1,1,4,10,9 };
+            double cem = 0;
+            double edediorta = 0;
+            for (int i = 0; i <a.Length; i++)
+            {
+                cem = cem + a[i];
+                edediorta = cem / a.Length;
+            }
+            Console.WriteLine("umumi cem: " + cem);
+            Console.WriteLine("ededi orta: " + edediorta);
+        }
+    }
+}
---
+++ tek_ededlerin_cemi.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 4, 4, 5, 6, 7, 8, 9, 10 };
+            int cem = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 == 1)
+                {
+                    cem += a[i];
+
+                    Console.Write("+" + a[i]);
+                }
+            }
+            Console.WriteLine("=" + cem);
+        }
+    }
+}
